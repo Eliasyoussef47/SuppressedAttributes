a98ec1ebcf181ba1c79f3a29be7146f3ec7af8aa
Elias Youssef <eliasyoussef47@gmail.com>
2024-11-10 19:31:18 +0100

    Bumps "Until Build".

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.eliasyoussef47.php.suppressedattributes"
-version = "0.1.2-beta"
+version = "0.1.3-beta"
 
 repositories {
 	mavenCentral()
@@ -36,7 +36,7 @@ tasks {
 
 	patchPluginXml {
 		sinceBuild.set("232")
-		untilBuild.set("242.*")
+		untilBuild.set("243.*")
 	}
 
 	signPlugin {
