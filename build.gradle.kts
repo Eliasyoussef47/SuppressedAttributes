a4589ee6826db9e4fe8385f51fd2b30ba223c1d9
Elias Youssef <eliasyoussef47@gmail.com>
2025-02-03 20:23:35 +0100

    Removes the "untilBuild" setting.
    Ups version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.eliasyoussef47.php.suppressedattributes"
-version = "0.1.3-beta"
+version = "0.1.4-beta"
 
 repositories {
 	mavenCentral()
@@ -36,7 +36,7 @@ tasks {
 
 	patchPluginXml {
 		sinceBuild.set("232")
-		untilBuild.set("243.*")
+//		untilBuild.set("251.*")
 	}
 
 	signPlugin {
